bac47e25e600ba1786f43ff092b3ae12e686f647
TheLordOfAllMuffins <nopika99@gmail.com>
2021-02-24 23:06:59 -0800

    Update test_parsing with private funcitons

--- test_parsing.py
+++ test_parsing.py
@@ -16,21 +16,21 @@ class class_parse_test_case(unittest.TestCase):
         }
 
     def test_class_parse(self):
-        self.assertEqual(parsing.eval_class_expression('C'), self.C)
-        self.assertEqual(parsing.eval_class_expression('V'), self.V)
+        self.assertEqual(parsing._eval_class_expression('C'), self.C)
+        self.assertEqual(parsing._eval_class_expression('V'), self.V)
 
     def test_sound_parse(self):
-        self.assertEqual(parsing.eval_class_expression('a,b,c'), parsing.eval_class_expression('abc'))
-        self.assertEqual(parsing.eval_class_expression('a,b,c'), parsing.sound_sequence('abc'))
+        self.assertEqual(parsing._eval_class_expression('a,b,c'), parsing._eval_class_expression('abc'))
+        self.assertEqual(parsing._eval_class_expression('a,b,c'), parsing.sound_sequence('abc'))
 
     def test_class_mult(self):
-        self.assertEqual(parsing.eval_class_expression('C*V'), self.C * self.V)
+        self.assertEqual(parsing._eval_class_expression('C*V'), self.C * self.V)
 
     def test_sounds_mult(self):
-        self.assertEqual(parsing.eval_class_expression('ptk*ʰ'), parsing.sound_sequence('ptk') * parsing.sound_sequence('ʰ'))
+        self.assertEqual(parsing._eval_class_expression('ptk*ʰ'), parsing.sound_sequence('ptk') * parsing.sound_sequence('ʰ'))
 
     def test_class_sound_mul(self):
-        self.assertEqual(parsing.eval_class_expression('C*ː'), self.C * parsing.sound_sequence('ː'))
+        self.assertEqual(parsing._eval_class_expression('C*ː'), self.C * parsing.sound_sequence('ː'))
 
 
 if __name__ == "__main__":
