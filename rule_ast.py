f3ffa161755ca2efbb1756dfe7a169914d637d80
le birb <nopika99@gmail.com>
2024-09-08 19:13:19 -0700

    Change parse_tokens to use match/case instead of ifs

--- rule_ast.py
+++ rule_ast.py
@@ -29,153 +29,154 @@ def parse_tokens(tokens: Iterable[token], sound_classes: dict[str, sound_class])
     finished_changes = False
 
     for token in tokens:
-        if token.type is token_type.sound:
-            parsing_stack.append(sound_node(token.string))
-        elif token.type is token_type.sound_class:
-            parsing_stack.append(sound_class_node(sound_classes[token.string]))
-        elif token.type is token_type.sound_class_number:
-            number = int(token.string)
-            class_node = parsing_stack.pop()
-            parsing_stack.append(numbered_sound_class_node(class_node, number))
+        match token.type:
+            case token_type.sound:
+                parsing_stack.append(sound_node(token.string))
+            case token_type.sound_class:
+                parsing_stack.append(sound_class_node(sound_classes[token.string]))
+            case token_type.sound_class_number:
+                number = int(token.string)
+                class_node = parsing_stack.pop()
+                parsing_stack.append(numbered_sound_class_node(class_node, number))
 
-        elif token.type is token_type.null_sound:
-            parsing_stack.append(sound_node(""))
-        elif token.type is token_type.word_border:
-            parsing_stack.append(word_border_node())
+            case token_type.null_sound:
+                parsing_stack.append(sound_node(""))
+            case token_type.word_border:
+                parsing_stack.append(word_border_node())
 
-        elif token.type is token_type.l_brace:
-            parsing_stack.append(_marker.brace)
-        elif token.type is token_type.l_paren:
-            parsing_stack.append(_marker.paren)
+            case token_type.l_brace:
+                parsing_stack.append(_marker.brace)
+            case token_type.l_paren:
+                parsing_stack.append(_marker.paren)
 
-        elif token.type is token_type.space and not finished_changes:
-            parsing_stack.append(_marker.space)
-        elif token.type is token_type.space:
-            pass # once changes are done, spaces are no longer meaningful
-        elif token.type is token_type.comma:
-            parsing_stack.append(_marker.comma)
+            case token_type.space if not finished_changes:
+                parsing_stack.append(_marker.space)
+            case token_type.space:
+                pass # once changes are done, spaces are no longer meaningful
+            case token_type.comma:
+                parsing_stack.append(_marker.comma)
 
-        elif token.type is token_type.ellipsis:
-            parsing_stack.append(repetition_node(parsing_stack.pop()))
+            case token_type.ellipsis:
+                parsing_stack.append(repetition_node(parsing_stack.pop()))
 
-        elif token.type is token_type.r_brace:
-            # parse a list of sound expressions, separated by commas
-            # could restrict this further but there's no present need to
-            expressions: list[expression_node] = []
-            curr_expression: list[element_node] = []
-            # take everything up to the open brace (and discard the brace, too)
-            while (element := parsing_stack.pop()) is not _marker.brace:
-                if element is _marker.comma:
-                    # reverse since we pulled things off in reverse
-                    curr_expression.reverse()
-                    expressions.append(expression_node(curr_expression))
-                    curr_expression = []
-                elif element is _marker.space:
-                    # intentionally skip spaces within braces
-                    pass
+            case token_type.r_brace:
+                # parse a list of sound expressions, separated by commas
+                # could restrict this further but there's no present need to
+                expressions: list[expression_node] = []
+                curr_expression: list[element_node] = []
+                # take everything up to the open brace (and discard the brace, too)
+                while (element := parsing_stack.pop()) is not _marker.brace:
+                    if element is _marker.comma:
+                        # reverse since we pulled things off in reverse
+                        curr_expression.reverse()
+                        expressions.append(expression_node(curr_expression))
+                        curr_expression = []
+                    elif element is _marker.space:
+                        # intentionally skip spaces within braces
+                        pass
+                    else:
+                        curr_expression.append(element)
+                # make sure to include the leftmost expression as well
+                curr_expression.reverse()
+                expressions.append(expression_node(curr_expression))
+                expressions.reverse()
+                parsing_stack.append(sound_list_node(expressions))
+
+            case token_type.r_paren:
+                # parse one expression back to the open parenthesis
+                expression: list[element_node] = []
+                # take everything up to the open parenthesis (and discard it the paren)
+                while (element := parsing_stack.pop()) is not _marker.paren:
+                    expression.append(element)
+                expression.reverse()
+                parsing_stack.append(optional_node(expression_node(expression)))
+
+            case token_type.arrow:
+                # we've hit the end of one section but not of all of the changes
+                parsing_stack.append(_parse_expression_list(parsing_stack))
+
+            case token_type.neg_slash | token_type.pos_slash | token_type.eol if not finished_changes:
+                # we've hit the end of the changes
+                # get the last expression list
+                parsing_stack.append(_parse_expression_list(parsing_stack))
+                # then collect the lists into change_nodes
+                changes: list[change_node] = []
+                while parsing_stack:
+                    curr_repl = parsing_stack.pop()
+                    peek = parsing_stack[-1]
+                    if isinstance(peek, expression_list_node):
+                        changes.append(change_node(target = peek, replacement = curr_repl))
+                    elif peek is _marker.stack_start:
+                        # get rid of the start marker; it was only used to make peeking
+                        # here and in _parse_expression_list easier
+                        parsing_stack.pop()
+                        break
+                    else:
+                        raise RuntimeError(f"Parsing stack in invalid state! Raw {type(peek)} left on stack during change collection.")
                 else:
-                    curr_expression.append(element)
-            # make sure to include the leftmost expression as well
-            curr_expression.reverse()
-            expressions.append(expression_node(curr_expression))
-            expressions.reverse()
-            parsing_stack.append(sound_list_node(expressions))
+                    raise RuntimeError("Parsing stack in invalid state! Start marker was popped before changes finished parsing.")
+                finished_changes = True
+                changes.reverse()
+                parsing_stack.append(changes)
 
-        elif token.type is token_type.r_paren:
-            # parse one expression back to the open parenthesis
-            expression: list[element_node] = []
-            # take everything up to the open parenthesis (and discard it the paren)
-            while (element := parsing_stack.pop()) is not _marker.paren:
-                expression.append(element)
-            expression.reverse()
-            parsing_stack.append(optional_node(expression_node(expression)))
+                # add a marker to let the parser know later which kind of environment is currently being parsed
+                if token.type is token_type.pos_slash:
+                    parsing_stack.append(_marker.pos_env)
+                elif token_type is token_type.neg_slash:
+                    parsing_stack.append(_marker.neg_env)
 
-        elif token.type is token_type.arrow:
-            # we've hit the end of one section but not of all of the changes
-            parsing_stack.append(_parse_expression_list(parsing_stack))
+            case token_type.underscore:
+                expression = []
+                while parsing_stack[-1] not in (_marker.pos_env, _marker.neg_env):
+                    expression.append(parsing_stack.pop())
+                expression.reverse()
+                parsing_stack.append(expression_node(expression))
 
-        elif token.type in (token_type.neg_slash, token_type.pos_slash, token_type.eol) and not finished_changes:
-            # we've hit the end of the changes
-            # get the last expression list
-            parsing_stack.append(_parse_expression_list(parsing_stack))
-            # then collect the lists into change_nodes
-            changes: list[change_node] = []
-            while parsing_stack:
-                curr_repl = parsing_stack.pop()
-                peek = parsing_stack[-1]
-                if isinstance(peek, expression_list_node):
-                    changes.append(change_node(target = peek, replacement = curr_repl))
-                elif peek is _marker.stack_start:
-                    # get rid of the start marker; it was only used to make peeking
-                    # here and in _parse_expression_list easier
-                    parsing_stack.pop()
-                    break
-                else:
-                    raise RuntimeError(f"Parsing stack in invalid state! Raw {type(peek)} left on stack during change collection.")
-            else:
-                raise RuntimeError("Parsing stack in invalid state! Start marker was popped before changes finished parsing.")
-            finished_changes = True
-            changes.reverse()
-            parsing_stack.append(changes)
+            case token_type.neg_slash | token_type.pos_slash:
+                # we've hit the end of an environment
+                # parse the post-environment expression and add it to the stack, then make an environment node
+                expression = []
+                while not isinstance(parsing_stack[-1], expression_node):
+                    expression.append(parsing_stack.pop())
+                expression.reverse()
+                post_expression = expression_node(expression)
+                pre_expression = parsing_stack.pop()
+                is_positive = parsing_stack.pop() is _marker.pos_env
+                parsing_stack.append(environment_node(pre_expression, post_expression, is_positive))
 
-            # add a marker to let the parser know later which kind of environment is currently being parsed
-            if token.type is token_type.pos_slash:
-                parsing_stack.append(_marker.pos_env)
-            elif token.type is token_type.neg_slash:
-                parsing_stack.append(_marker.neg_env)
+                # add a marker to let the parser know later which kind of environment it's parsing next
+                if token.type is token_type.pos_slash:
+                    parsing_stack.append(_marker.pos_env)
+                elif token.type is token_type.neg_slash:
+                    parsing_stack.append(_marker.neg_env)
 
-        elif token.type is token_type.underscore:
-            expression = []
-            while parsing_stack[-1] not in (_marker.pos_env, _marker.neg_env):
-                expression.append(parsing_stack.pop())
-            expression.reverse()
-            parsing_stack.append(expression_node(expression))
+            case token_type.eol:
+                # we've hit the end of all things, and also know that the latest things are environments
+                # as above, make the latest environment node
+                # parse the post-environment expression and add it to the stack, then make an environment node
+                expression = []
+                while not isinstance(parsing_stack[-1], expression_node):
+                    expression.append(parsing_stack.pop())
+                expression.reverse()
+                post_expression = expression_node(expression)
+                pre_expression = parsing_stack.pop()
+                is_positive = parsing_stack.pop() is _marker.pos_env
+                parsing_stack.append(environment_node(pre_expression, post_expression, is_positive))
 
-        elif token.type in (token_type.neg_slash, token_type.pos_slash):
-            # we've hit the end of an environment
-            # parse the post-environment expression and add it to the stack, then make an environment node
-            expression = []
-            while not isinstance(parsing_stack[-1], expression_node):
-                expression.append(parsing_stack.pop())
-            expression.reverse()
-            post_expression = expression_node(expression)
-            pre_expression = parsing_stack.pop()
-            is_positive = parsing_stack.pop() is _marker.pos_env
-            parsing_stack.append(environment_node(pre_expression, post_expression, is_positive))
+                # then collect all environment nodes into an environment list node
+                pos_envs: list[environment_node] = []
+                neg_envs: list[environment_node] = []
+                while isinstance(parsing_stack[-1], environment_node):
+                    env_node: environment_node = parsing_stack.pop()
+                    if env_node.is_positive:
+                        pos_envs.append(env_node)
+                    else:
+                        neg_envs.append(env_node)
+                parsing_stack.append(pos_envs)
+                parsing_stack.append(neg_envs)
 
-            # add a marker to let the parser know later which kind of environment it's parsing next
-            if token.type is token_type.pos_slash:
-                parsing_stack.append(_marker.pos_env)
-            elif token.type is token_type.neg_slash:
-                parsing_stack.append(_marker.neg_env)
-
-        elif token.type is token_type.eol:
-            # we've hit the end of all things, and also know that the latest things are environments
-            # as above, make the latest environment node
-            # parse the post-environment expression and add it to the stack, then make an environment node
-            expression = []
-            while not isinstance(parsing_stack[-1], expression_node):
-                expression.append(parsing_stack.pop())
-            expression.reverse()
-            post_expression = expression_node(expression)
-            pre_expression = parsing_stack.pop()
-            is_positive = parsing_stack.pop() is _marker.pos_env
-            parsing_stack.append(environment_node(pre_expression, post_expression, is_positive))
-
-            # then collect all environment nodes into an environment list node
-            pos_envs: list[environment_node] = []
-            neg_envs: list[environment_node] = []
-            while isinstance(parsing_stack[-1], environment_node):
-                env_node: environment_node = parsing_stack.pop()
-                if env_node.is_positive:
-                    pos_envs.append(env_node)
-                else:
-                    neg_envs.append(env_node)
-            parsing_stack.append(pos_envs)
-            parsing_stack.append(neg_envs)
-
-        else:
-            raise NotImplementedError(f"Unrecognized token {token} encountered while parsing.")
+            case _:
+                raise NotImplementedError(f"Unrecognized token {token} encountered while parsing.")
 
     # finally, the parsing stack should now look like [list[changes], list[environment_node], list[environment_node]]
     return rule_node(*parsing_stack)
